487b4dc1d0f5864c16cf8751c1fff48c2d68118b
Dan Gillespie <danielg@umd.edu>
2014-11-19 06:42:39 -0800

    added mongo persistance

--- members.py
+++ members.py
@@ -1,14 +1,35 @@
 import urllib.request
 from bs4 import BeautifulSoup
 import re
+from pymongo import MongoClient
 
 # FB Setup
 tag = 'fbProfileBrowserListContainer'
 groupId = "{insert group}"
 cookieText = "{insert cookie}"
 
+client = MongoClient()
+coll = client[groupId].members
+count = 0
+
 def handleAdd(obj):
-    print (obj.a.text)
+    user = dict()
+    bio = obj.div
+    user['name'] = obj.a.text
+    for d in obj.findAll("div"):
+        if "fsl" in d["class"]:
+            url = d.a["data-hovercard"]
+            userid = midRegex('id=(.*?)&extraget', url)
+            user['url'] = d.a['href']
+            user['userid'] = userid
+        if "_17tq" in d["class"]:
+            user['occupation']= d.text
+        if "fsm" in d["class"]:
+            user['joined'] = float(d.abbr['data-utime'])
+            if d.text.startswith("Added by"):
+                user['addedby'] = d.abbr.previousSibling[9:-1]
+    coll.insert(user)
+    print (user['name'])
 
 def retrieve(url):
     request = urllib.request.Request(url)
